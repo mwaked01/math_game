788ff61a24a76e4e063491286dceed6272fbab80
Mohamed Waked <mwaked01@gmail.com>
2023-12-16 05:39:38 -0500

    player class

--- player.rb
+++ player.rb
@@ -1,3 +1,14 @@
+require 'active_support/all'
 class Player
+  attr_accessor :name, :score 
   
+  def initialize(name)
+    @name = name
+    @score = 3
+  end
+
 end
+
+player1 = Player.new("Player 1")
+player1.score -= 1
+puts (player1.score)
